f3ad72cb7deab92e9374ca0fc7a7459e944fe1ae
tkhahns <111437863+tkhahns@users.noreply.github.com>
2023-07-18 17:28:58 +0800

    Completed add to User Portfolio

--- src/components/searchInput/userPortfolio.tsx
+++ src/components/searchInput/userPortfolio.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 
-interface StockItem {
-  stockCode: string;
-  companyName: string;
+export interface StockItem {
+  name: string;
+  symbol: string;
   country: string;
-  stockType: string;
+  type: string;
 }
 
 interface UserPortfolioProps {
   userPortfolio: StockItem[];
+  onRemoveStock: (stock: StockItem) => void;
 }
 
-const UserPortfolio: React.FC<UserPortfolioProps> = ({ userPortfolio }) => (
-  <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
-    <div>
-      <h2>User Portfolio</h2>
-      <ul>
-        {userPortfolio.map((item) => (
-          <li key={item.stockCode}>
-            {item.stockCode} - {item.companyName} - {item.stockType} - {item.country}
-          </li>
-        ))}
-      </ul>
-    </div>
+const UserPortfolio: React.FC<UserPortfolioProps> = ({ userPortfolio, onRemoveStock }) => (
+<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+  <div>
+    <h2 style={{ textAlign: 'center' }}>User Portfolio</h2>
+    <ul style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 0 }}>
+      {userPortfolio.map((item, index) => (
+        <li key={index} style={{ textAlign: 'center', margin: '10px 0' }}>
+          {item.symbol} - {item.name} - {item.country} - {item.type} 
+          <button onClick={() => onRemoveStock(item)}>Remove from Portfolio</button>
+        </li>
+      ))}
+    </ul>
   </div>
+</div>
+
 );
 
 export default UserPortfolio;
--- src/pages/stockDetails.tsx
+++ src/pages/stockDetails.tsx
@@ -1,25 +1,39 @@
-import { useLocation } from "react-router-dom"
-import { Stock } from "../components/searchInput/searchInput"
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Stock } from '../components/searchInput/searchInput';
 
 export const StockDetails = () => {
-    const location = useLocation();
-    const stockInfo = location.state.stock;
-    const country = stockInfo.country;
-    const symbol = stockInfo.symbol;
-    const currency = stockInfo.currency;
-    const exchange = stockInfo.exchange;
-    const micCode = stockInfo.mic_code;
-    const type = stockInfo.type;
-    const name = stockInfo.name;
-    return (
-        <div>
-            <p>{country}</p>
-            <p>{symbol}</p>
-            <p>{currency}</p>
-            <p>{exchange}</p>
-            <p>{micCode}</p>
-            <p>{type}</p>
-            <p>{name}</p>
-        </div>
-    )
-}
+  const location = useLocation();
+  const stockInfo = location.state.stock;
+  const [addedToPortfolio, setAddedToPortfolio] = useState(false);
+  const navigate = useNavigate();
+
+  const addToPortfolio = () => {
+    const userPortfolio = JSON.parse(localStorage.getItem('userPortfolio') || '[]');
+    userPortfolio.push(stockInfo);
+    localStorage.setItem('userPortfolio', JSON.stringify(userPortfolio));
+    setAddedToPortfolio(true);
+  };
+
+  const country = stockInfo.country;
+  const symbol = stockInfo.symbol;
+  const currency = stockInfo.currency;
+  const exchange = stockInfo.exchange;
+  const micCode = stockInfo.mic_code;
+  const type = stockInfo.type;
+  const name = stockInfo.name;
+
+  return (
+    <div>
+      <p>{country}</p>
+      <p>{symbol}</p>
+      <p>{currency}</p>
+      <p>{exchange}</p>
+      <p>{micCode}</p>
+      <p>{type}</p>
+      <p>{name}</p>
+      {!addedToPortfolio && <button onClick={addToPortfolio}>Add to User Portfolio</button>}
+      <button onClick={() => navigate('/home')}>Go to Home</button>
+    </div>
+  );
+};
