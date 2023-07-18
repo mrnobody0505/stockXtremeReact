e0b4a77ee6f232f0801eb69c6511611edbad0ee6
Do Gia Hien <giahien0505@gmail.com>
2023-07-18 21:00:03 +0700

    charting feature 1st vers

--- src/pages/stockDetails.tsx
+++ src/pages/stockDetails.tsx
@@ -1,25 +1,33 @@
 import { useLocation } from "react-router-dom"
 import { Stock } from "../components/searchInput/searchInput"
+import StockChart from "../components/charting/stockChart";
 
 export const StockDetails = () => {
     const location = useLocation();
     const stockInfo = location.state.stock;
-    const country = stockInfo.country;
+    //const country = stockInfo.country;
     const symbol = stockInfo.symbol;
-    const currency = stockInfo.currency;
-    const exchange = stockInfo.exchange;
-    const micCode = stockInfo.mic_code;
-    const type = stockInfo.type;
-    const name = stockInfo.name;
+    const apiKey = "efc815ecdf7973429abcd4c791f93fe7"
     return (
-        <div>
-            <p>{country}</p>
-            <p>{symbol}</p>
-            <p>{currency}</p>
-            <p>{exchange}</p>
-            <p>{micCode}</p>
-            <p>{type}</p>
-            <p>{name}</p>
-        </div>
-    )
+      <div style={{ width: '100%' }}>
+        <h1>{symbol}</h1>
+        <StockChart apiKey={apiKey} symbol={symbol} />
+      </div>
+    );
+    // const currency = stockInfo.currency;
+    // const exchange = stockInfo.exchange;
+    // const micCode = stockInfo.mic_code;
+    // const type = stockInfo.type;
+    // const name = stockInfo.name;
+    // return (
+    //     <div>
+    //         <p>{country}</p>
+    //         <p>{symbol}</p>
+    //         <p>{currency}</p>
+    //         <p>{exchange}</p>
+    //         <p>{micCode}</p>
+    //         <p>{type}</p>
+    //         <p>{name}</p>
+    //     </div>
+    // )
 }
