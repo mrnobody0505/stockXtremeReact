cff1a7a41622fa78b2309c377b2aeb1f13c111cd
tkhahns <111437863+tkhahns@users.noreply.github.com>
2023-07-24 14:32:02 +0800

    Added CSS elements

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,9 +3,11 @@ import { Link, Route, Routes } from 'react-router-dom';
 import Navbar from '../components/navbar/navbar';
 import UserPortfolio, { StockItem } from '../components/searchInput/userPortfolio';
 import UserProfile from '../components/navbar/userProfile'; // Import the UserProfile component
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Home = () => {
   const [userPortfolio, setUserPortfolio] = useState<StockItem[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const storedUserPortfolio = JSON.parse(localStorage.getItem('userPortfolio') || '[]');
@@ -26,6 +28,12 @@ export const Home = () => {
       <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <UserPortfolio />
       </div>
+      <button
+        onClick={() => navigate('/')}
+        style={{ background: 'darkblue', color: 'white', fontSize: '14px', padding: '8px 16px', borderRadius: '4px' }}
+      >
+        Log out
+      </button>
     </div>
   );
 };
